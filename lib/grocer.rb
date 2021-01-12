4c2b9eac7e56fd13d587f1b3f2cc72bcb1df9df9
Menhaj Sharaf <menhajuddin.sharaf@gmail.com>
2021-01-12 22:53:25 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,65 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+def find_item_by_name_in_collection(name, collectionOfItems)
+    foundItem = nil
+    collectionOfItems.each do |element|
+        element.each do |key, value|
+            if (value == name)
+                foundItem = element
+                # puts foundItem
+            end
+        end
+    end
+    foundItem
+  end
 
-end
+# def consolidate_cart(cart)
+#     cart.each do |element|
+#         element.map do |key, value|
+#             # puts value
+#         end
+#     end
+#     updateList = cart.tally
+#     mystList = {}
+#     consList = []
+#     updateList.map do |key, value|
+#         # puts "#{key}     #{value}"
+#         # temp = {:count => nil}
+#         temp = {}
+#         key.each do |k, v|
+#             temp[k] = v
+#         end
+#         temp[:count] = value
+#         consList.push(temp)
+#     end
+#     # puts mystList
+#     consList
+# end
+
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-end
-
-
+    
+    updated_cart = Array.new
+    cart_index = 0
   
+    while cart_index < cart.size do
+      current_item = cart[cart_index]
+      if ( find_item_by_name_in_collection( current_item[:item], updated_cart ) == nil)
+        current_item[:count] = 1
+        updated_cart.push(current_item)
+      else
+        increment_count_of_item( updated_cart, current_item[:item] )
+      end
+      cart_index += 1
+    end
+    updated_cart
+  end
+  def increment_count_of_item (cart, item_name)
+    cart_index = 0
+    while cart_index < cart.size do
+      current_item = cart[cart_index]
+      if ( current_item[:item] == item_name )
+        current_item[:count] += 1
+      end
+      cart_index += 1
+    end
+    cart
+  end
