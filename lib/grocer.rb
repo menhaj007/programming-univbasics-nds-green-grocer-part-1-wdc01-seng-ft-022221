24a01fc43315409dba103dc4fa5403a70f656723
Menhajuddin Sharaf <menhajuddin.sharaf@gmail.com>
2021-01-14 01:22:10 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,65 +1,79 @@
-def find_item_by_name_in_collection(name, collectionOfItems)
+grocery_shelf = [
+    { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
+    { :item => "CANNED CORN", :price => 2.50, :clearance => false },
+    { :item => "SALSA", :price => 1.50, :clearance => false },
+    { :item => "TORTILLAS", :price => 2.00, :clearance => false },
+    { :item => "HOT SAUCE", :price => 1.75, :clearance => false },
+    { :item => "CANNED BEANS", :price => 3.00, :clearance => true }
+  ]
+  grocer = [
+    {:item => "AVOCADO", :price => 3.00, :clearance => true},
+    {:item => "KALE", :price => 3.00, :clearance => false},
+    {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+    {:item => "ALMONDS", :price => 9.00, :clearance => false},
+    {:item => "TEMPEH", :price => 3.00, :clearance => true},
+    {:item => "CHEESE", :price => 6.50, :clearance => false},
+    {:item => "BEER", :price => 13.00, :clearance => false},
+    {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+    {:item => "BEETS", :price => 2.50, :clearance => false},
+    {:item => "SOY MILK", :price => 4.50, :clearance => true},
+    {:item => "SOY MILK", :price => 4.50, :clearance => true}
+  ]
+
+  def find_item_by_name_in_collection(name, collectionOfItems)
     foundItem = nil
     collectionOfItems.each do |element|
         element.each do |key, value|
             if (value == name)
                 foundItem = element
-                # puts foundItem
             end
         end
     end
     foundItem
   end
 
-# def consolidate_cart(cart)
-#     cart.each do |element|
-#         element.map do |key, value|
-#             # puts value
-#         end
-#     end
-#     updateList = cart.tally
-#     mystList = {}
-#     consList = []
-#     updateList.map do |key, value|
-#         # puts "#{key}     #{value}"
-#         # temp = {:count => nil}
-#         temp = {}
-#         key.each do |k, v|
-#             temp[k] = v
-#         end
-#         temp[:count] = value
-#         consList.push(temp)
-#     end
-#     # puts mystList
-#     consList
-# end
+#   puts find_item_by_name_in_collection("BEErTS", grocer)
+#   puts find_item_by_name_in_collection("CANNED CORN", grocery_shelf)
 
 
-def consolidate_cart(cart)
-    
-    updated_cart = Array.new
-    cart_index = 0
+unconsolidated_cart = [
+    {:item => "AVOCADO", :price => 3.00, :clearance => true },
+    {:item => "AVOCADO", :price => 3.00, :clearance => true },
+    {:item => "KALE", :price => 3.00, :clearance => false}
+  ]
   
-    while cart_index < cart.size do
-      current_item = cart[cart_index]
-      if ( find_item_by_name_in_collection( current_item[:item], updated_cart ) == nil)
-        current_item[:count] = 1
-        updated_cart.push(current_item)
-      else
-        increment_count_of_item( updated_cart, current_item[:item] )
-      end
-      cart_index += 1
-    end
-    updated_cart
-  end
-  def increment_count_of_item (cart, item_name)
-    cart_index = 0
-    while cart_index < cart.size do
-      current_item = cart[cart_index]
+  result = [
+                {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 2},
+                {:item => "KALE", :price => 3.00, :clearance => false, :count => 1}
+            ]
+  def update_counts (item_name, half_cosolidated_cart)
+    index = 0
+    while index < half_cosolidated_cart.size do
+      current_item = half_cosolidated_cart[index]
       if ( current_item[:item] == item_name )
         current_item[:count] += 1
       end
-      cart_index += 1
+      index += 1
     end
-    cart
+    half_cosolidated_cart
   end
+
+  def consolidate_cart(unconsolidated_cart)
+    consolidated_cart = Array.new
+    index = 0
+    while index < unconsolidated_cart.length do
+        current_item = unconsolidated_cart[index]
+        # puts current_item[:item]
+        if (find_item_by_name_in_collection(current_item[:item], consolidated_cart) == nil)
+          current_item[:count] = 1
+          consolidated_cart.push(current_item)
+        else 
+          update_counts(current_item[:item], consolidated_cart)
+        end
+        index += 1
+    end
+    consolidated_cart
+  end
+  puts consolidate_cart(unconsolidated_cart)
+
+  
